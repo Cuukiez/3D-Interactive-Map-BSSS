65e16e4a7eb90620c742e3411d4ba93f986548b9
Cuukiez <leojialinlu@gmail.com>
2025-03-30 14:04:48 -0700

    Update: Fixed models and added features

--- main.js
+++ main.js
@@ -4,81 +4,69 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { MapControls } from 'three/addons/controls/MapControls.js';
 
+
+//* Initialization
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.y = 10
 
 const renderer = new THREE.WebGLRenderer({alpha: true});
 
-//let mouseX = window.innerWidth / 2;
-//let mouseY = window.innerHeight / 2;
-
 let object;
+let objToLoad = 'Floor1'
 
 let controls = new MapControls( camera, renderer.domElement );
 
-//controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+const topLight = new THREE.DirectionalLight(0xffffff, objToLoad == 'snorlax' ? 0 : 5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+
+const gridHelper = new THREE.GridHelper(350, 50);
+
+var zoomIn = document.getElementById( 'zoom-in' );
+var zoomOut = document.getElementById( 'zoom-out' );
+		
+const loader = new GLTFLoader();
+
+
+//* Components
+
+camera.position.set(0, 250, 200);
+//camera.position.y = 250;
+
+topLight.position.set(200, 700, 200);
+topLight.castShadow = true;
+
+gridHelper.position.y = -1;
+gridHelper.position.x = 1
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.getElementById('container3D').appendChild(renderer.domElement);
+
+scene.add(
+    ambientLight,
+    topLight,
+    gridHelper,
+);
+
+
+//* Controls
 
 controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
 controls.dampingFactor = 0.05;
 
 controls.screenSpacePanning = false;
 
-controls.minDistance = 10;
+controls.minDistance = 100;
 controls.maxDistance = 500;
 
 controls.maxPolarAngle = Math.PI / 3.2;
 
-const loader = new GLTFLoader();
+controls.target.set(0,0,0);
 
-let objToLoad = 'BSSS'
 
-const gridHelper = new THREE.GridHelper(200, 50);
-gridHelper.position.y = -1;
-scene.add( gridHelper );
-
-function load() {
-    loader.load(
-        `models/${objToLoad}/scene.gltf`, (gltf) => {
-    
-        object = gltf.scene;
-        scene.add(object);
-    
-        const materials = {};
-        materials[ 'wireframe' ] = new THREE.MeshBasicMaterial( { wireframe: true } );
-    
-    });
-}
-
-load();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.getElementById('container3D').appendChild(renderer.domElement);
-
-camera.position.z = 25;
-
-const topLight = new THREE.DirectionalLight(0xffffff, objToLoad == 'snorlax' ? 0 : 5);
-topLight.position.set(200, 700, 200);
-topLight.castShadow = true;
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 2);
-scene.add(
-    ambientLight,
-    topLight,
-);
-
-function animate() {
-    requestAnimationFrame(animate);
-
-    //object.rotation.x += 0.003;
-    //object.rotation.y += -0.0025;
-    
-    controls.update();
-
-    renderer.render(scene, camera);
-}
+//* Event Listeners
 
 window.addEventListener("resize", function(){
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -91,15 +79,97 @@ window.addEventListener("mousedown", event =>{
         scene.remove(object);
         if (objToLoad == 'teapot'){
             objToLoad = 'snorlax';
+
             load();
+            console.log(objToLoad);
+
         }else if (objToLoad == 'snorlax'){
-            objToLoad = 'BSSS';
+            objToLoad = 'Floor1';
+            
+            controls.target.set(0,0,0);
+            camera.position.set(0, 250, 200);
+
             load();
+            console.log(objToLoad);
+
+        }else if (objToLoad == 'Floor1'){
+            objToLoad = 'Floor2';
+
+            controls.target.set(0,30,20);
+            camera.position.set(0, 250, 200);
+
+            load();
+            console.log(objToLoad);
+
         }else{
             objToLoad = 'teapot';
+
             load();
+            console.log(objToLoad);
         }
     }
 })
+
+zoomIn.addEventListener( 'click', onFloor1, false );
+zoomOut.addEventListener( 'click', onFloor2, false );
+
+
+//* Functions
+
+function load() {
+    loader.load(
+        `./models/${objToLoad}/scene.gltf`, (gltf) => {
+    
+        object = gltf.scene;
+        object.position.set( -5 , 0 , 0)
+        object.rotation.y = Math.PI
+        scene.add(object);
+
+
+        const materials = {};
+        materials[ 'wireframe' ] = new THREE.MeshBasicMaterial( { wireframe: true } );
+    
+    });
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+
+    renderer.render(scene, camera);
+}
+
+function onFloor1() {
+
+	objToLoad = 'Floor1';
+            
+    controls.target.set(0,0,0);
+    camera.position.set(0, 250, 200);
+
+    load();
+
+    scene.remove(object);
+
+    console.log(objToLoad);
+
+}
+
+function onFloor2() {
+
+	objToLoad = 'Floor2';
+
+    load();
+
+    scene.remove(objToLoad);
+
+    controls.target.set(0,30,20);
+    camera.position.set(0, 250, 200);
+
+
+}
+
+//* Start
+
+load();
 
 animate();
